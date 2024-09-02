486c16c350efa4ec6b90275ee7006685490dec90
Maksym <139382157+shunpocode@users.noreply.github.com>
2024-09-02 16:19:58 +0300

    Delete tests directory

--- tests/lib.test.ts
+++
@@ -1,3 +0,0 @@
-import { Validation } from "../lib.js";
-
-Validation
--- tests/test.js
+++
@@ -1,5 +0,0 @@
-import { Validation } from "../lib.js";
-
-console.log(Validation.isInteger("asda"));
-console.log(Validation.isInteger("123"));
-console.log(Validation.isInteger(123));
