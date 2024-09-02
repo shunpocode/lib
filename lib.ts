09b3f2c0bcf6ee8ffef2e2f550fc6a49e2163015
Maksym <139382157+shunpocode@users.noreply.github.com>
2024-09-02 16:20:42 +0300

    Delete lib.ts

--- lib.ts
+++
@@ -1,6 +0,0 @@
-import Input from "./libs/InputClass";
-import Validation from "./libs/InputValidation";
-import Observer from "./libs/Observer";
-import ScrollBar from "./libs/ScrollBarClass";
-
-export { Input, Validation, Observer, ScrollBar };
